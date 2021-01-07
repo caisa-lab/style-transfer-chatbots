d347754040f427ad27b34650307db535f65c355e
Philipp Nothvogel <phil-n@outlook.com>
2021-01-07 14:26:47 +0100

    change: sampling multiple sentences at once.

--- web-demo/sample_service.py
+++ web-demo/sample_service.py
@@ -14,12 +14,15 @@ from watchdog.events import FileSystemEventHandler
 from style_paraphrase.inference_utils import GPT2Generator
 from profanity_filter import ProfanityFilter
 
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
 pf = ProfanityFilter()
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--seed', type=int, default=34,
-                    help='Random seed to use for selecting inputs.')
+#parser.add_argument('--seed', type=int, default=34,
+#                    help='Random seed to use for selecting inputs.')
 args = parser.parse_args()
 
 with open("config.json", "r") as f:
@@ -35,17 +38,17 @@ with torch.cuda.device(0):
     formality = GPT2Generator(OUTPUT_DIR + "/models/formality")
     #formality.gpt2_model.eval()
 
-next_key = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
 
 style_mapping = {
     "Formality": {"model": formality, "device": 0, "data_file": "formality"}
 }
 
 data_style_mapping = {
-    "coha_1890": {"data_file": "coha_1890-1910"},
-    "coha_1990": {"data_file": "coha_1990-2000"},
-    "Shakespeare": {"data_file": "shakespeare"},
-    "Conversational Speech": {"data_file": "switchboard"},
+    "aae": {"data_file": "aae"},
+    "bible": {"data_file": "bible"},
+    "english_tweets": {"data_file": "english_tweets"},
+    "lyrics": {"data_file": "lyrics"},
+    "switchboard": {"data_file": "switchboard"},
 }
 
 data_style_list = list(data_style_mapping.keys())
@@ -63,17 +66,19 @@ else:
 
     all_sents_clean = {}
     for style, sents in all_sents.items():
-        all_sents_clean[style] = [x.strip().strip("\"").strip("\'").strip() for x in tqdm.tqdm(sents) if pf.is_clean(x) and len(x.split()) < 25]
+        #all_sents_clean[style] = [x.strip().strip("\"").strip("\'").strip() for x in tqdm.tqdm(sents) if pf.is_clean(x) and len(x.split()) < 25]
+        all_sents_clean[style] = [x.strip().strip("\"").strip("\'").strip() for x in tqdm.tqdm(sents) if len(x.split()) < 25]
 
     with open(OUTPUT_DIR + "/all_styles_clean.pkl", "wb") as f:
         pickle.dump(all_sents_clean, f)
 
-random.seed(args.seed)
+#random.seed(args.seed)
 
 
 # class EventHandler(FileSystemEventHandler):
 #     def on_any_event(self, event):
 def generation_service():
+    next_key = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
     data = {
         'random': True,
         'target_style': None
@@ -107,18 +112,19 @@ def generation_service():
                 model = style_mapping[data["target_style"]]["model"]
                 transferred_output = model.generate_batch(output_paraphrase, top_p=data["settings"]["top_p_style"])[0]
 
-        output_json.append({
-            "input_text": data["input_text"],
-            "paraphrase": output_paraphrase,
-            "style_transfer": transferred_output,
-            "target_style": data["target_style"]
-        })
+        for paraphrase, transferred in zip(output_paraphrase, transferred_output):
+            outputJson.append({
+                "input_text": data["input_text"],
+                "paraphrase": paraphrase,
+                "style_transfer": transferred,
+                "target_style": data["target_style"]
+            })
 
     outPath = OUTPUT_DIR + '/generated_outputs/' + next_key
     with open(outPath + '.json', 'w') as f:
-        f.write(json.dumps(output_json))
+        f.write(json.dumps(outputJson))
     
-    df = pd.DataFrame(output_json)
+    df = pd.DataFrame(outputJson)
     df.to_csv(outPath + '.csv')
     
 
@@ -126,7 +132,8 @@ def generation_service():
 if __name__ == "__main__":
     path = OUTPUT_DIR + "/generated_outputs"
     print(path)
-    generation_service()
+    for _ in range(10):
+        generation_service()
     # event_handler = EventHandler()
     # observer = Observer()
     # observer.schedule(event_handler, path, recursive=True)
