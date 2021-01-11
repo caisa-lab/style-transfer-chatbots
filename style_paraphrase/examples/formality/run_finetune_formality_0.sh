af5898d98a4015d4316c33fb837fe3bc8d3d56b8
Philipp Nothvogel <phil-n@outlook.com>
2021-01-11 12:15:17 +0100

    change: only keeping the top 3 checkpoints.

--- style_paraphrase/examples/formality/run_finetune_formality_0.sh
+++ style_paraphrase/examples/formality/run_finetune_formality_0.sh
@@ -48,7 +48,8 @@ python3 $BASE_DIR/run_lm_finetuning.py \
     --data_dir=$DATA_DIR \
     --save_steps 500 \
     --logging_steps 20 \
-    --save_total_limit -1 \
+    --do_delete_old \
+    --save_total_limit 3 \
     --evaluate_during_training \
     --num_train_epochs 10 \
     --gradient_accumulation_steps 1 \
