3f0b1d6bb1e0a3558d2937601fa4b84c0fc16384
Philipp Nothvogel <phil-n@outlook.com>
2021-03-11 14:31:17 +0100

    change: moved human eval to separate project

--- human-eval/generate_candidates.sh
+++
@@ -1,9 +0,0 @@
-GEN_SCRIPT="cmd-demo/predict_service.py"
- 
-for style in formal informal polite impolite receptive not_receptive offensive not_offensive; 
-do
-    python3 "$GEN_SCRIPT" \
-        --model $style \
-        --input_file "human-eval/input.csv" \
-        --output_file "human-eval/paraphrases.csv"
-done
