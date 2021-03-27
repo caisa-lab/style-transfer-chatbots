de9b421409cce5abd77394dc3cfb8ddef6ce9172
Philipp Nothvogel <phil-n@outlook.com>
2021-03-27 17:54:53 +0100

    add: support for excluding or caching intermediate paraphrases

--- cmd-demo/predict_service.py
+++ cmd-demo/predict_service.py
@@ -39,9 +39,21 @@ parser.add_argument('--filter_candidates', type=bool, default=False,
                     help='If set to true, will only save the candidate with the best perplexity.')
 parser.add_argument('--batch_size', type=int, default=16,
                     help='Batch size to use for predictions')
-parser.add_argument('--use_intermediate_paraphraser', type=bool, default=True,
+parser.add_argument('--use_diverse_paraphraser', dest='use_diverse_paraphraser', action='store_true',
                     help='Controls if a diverse paraphrasing model will be used before feeding the sentences to the stylistic paraphraser.')
-
+parser.add_argument('--no_diverse_paraphraser', dest='use_diverse_paraphraser', action='store_false',
+                    help='Controls if a diverse paraphrasing model will be used before feeding the sentences to the stylistic paraphraser.')
+parser.set_defaults(use_diverse_paraphraser=True)
+parser.add_argument('--use_cached_intermediate', dest='use_cached_intermediate', action='store_true',
+                    help='Use cached intermediate paraphrases to speed up computation.')
+parser.add_argument('--no_cached_intermediate', dest='use_cached_intermediate', action='store_false',
+                    help='Do not use cached intermediate paraphrases to speed up computation.')
+parser.set_defaults(use_cached_intermediate=False)
+parser.add_argument('--filter_column', type=str, default='',
+                    help='Can be used to only paraphrase parts of the input file where the given column is set to "True".')
+parser.add_argument('--filter_true', dest='filter_value', action='store_true')
+parser.add_argument('--filter_false', dest='filter_value', action='store_false')
+parser.set_defaults(filter_value=True)
 
 args = parser.parse_args()
 
@@ -49,11 +61,13 @@ with torch.cuda.device(0):
     print('Loading target style model:', args.model)
     model = GPT2Generator(os.path.join(OUTPUT_DIR, 'models', args.model))
     model.gpt2_model.eval()
+            
+def loadParaphraser():
+    print('Loading paraphraser...')
+    paraphraser = GPT2Generator(os.path.join(OUTPUT_DIR, 'models', 'paraphraser_gpt2_large'), upper_length='same_5')
+    paraphraser.gpt2_model.eval()
+    return paraphraser
 
-    if (args.use_intermediate_paraphraser):
-        print('Loading paraphraser...')
-        paraphraser = GPT2Generator(os.path.join(OUTPUT_DIR, 'models', 'paraphraser_gpt2_large'), upper_length='same_5')
-        paraphraser.gpt2_model.eval()
 
 style_mapping = {
     args.model: {'model': model, 'device': 0, 'data_file': args.model}
@@ -72,7 +86,11 @@ def main():
 
     print('Input file:', args.input_file)
     print('Using text from the column named:', args.text_column)
+    if (args.filter_column and len(args.filter_column) > 0):
+        print('Filtering input based on column named:', args.filter_column)
+
     inDf = pd.read_csv(args.input_file)
+    inDf = inDf.loc[inDf[args.filter_column] == args.filter_value]
     numOfSamples = args.num_of_candidates
     sentenceIndex = 0
     print('Tokenizing {} samples...'.format(len(inDf[textCol])))
@@ -85,45 +103,63 @@ def main():
                 sentenceIndices.append(sentenceIndex)
             sentenceIndex += 1
 
-    print('Generating intermediate paraphrases of {} sentences...'.format(len(input_samples)))
     batchSize = args.batch_size
     with torch.no_grad():
         output_paraphrase = []
         paraphrase_perplexities = []
-        if (args.use_intermediate_paraphraser):
-            for i in tqdm(range(0, len(input_samples), batchSize)):
-                currentBatch = input_samples[i:i + batchSize]
-                with torch.cuda.device(0):
-                    currentParaphrases, currentScores = paraphraser.generate_batch(currentBatch, top_p=args.top_p_paraphrase, get_scores=True)
-                    currentPerplexities = 2 ** (-np.array(currentScores))
-                
-                output_paraphrase += currentParaphrases
-                paraphrase_perplexities += currentPerplexities.tolist()
-
-            # create df of intermediate / diverse paraphrases and save it
-            df = pd.DataFrame().assign(source_idx=sourceIndices, sentence_idx=sentenceIndices, original_sentence=input_samples, paraphrase=output_paraphrase, paraphrase_perplexity=paraphrase_perplexities)
-            df = df.sort_values(by=['sentence_idx', 'paraphrase_perplexity'])
+        df = None
+        if (args.use_diverse_paraphraser):
             intermediatePath = csvPath.replace('.csv', '_intermediate.csv')
-            if (os.path.isfile(intermediatePath)):
-                df.to_csv(intermediatePath, mode='a', header=False)
+            if (os.path.isfile(intermediatePath) and args.use_cached_intermediate):
+                # use cached version?
+                df = pd.read_csv(intermediatePath, index_col=0)
+                numOfSourceIndices = len(df['source_idx'].unique())
+                if (numOfSourceIndices != len(inDf)):
+                    errorMessage = 'Cached intermediate paraphrases do not match the input df! '
+                    errorMessage += 'Please either delete the following file: \n'
+                    errorMessage += intermediatePath
+                    errorMessage += 'Or launch this script with the "--no_cached_intermediate" argument!'
+                    print('Error:')
+                    print(errorMessage)
+                    raise ValueError(errorMessage)
+                # else
+                print('Using cached version of intermediate paraphrases...')
             else:
-                df.to_csv(intermediatePath)
-            print('Saved intermediate / diverse paraphrases in', intermediatePath)
-        
-            # only use the most probable intermediate paraphrase
-            if (args.top_p_paraphrase > 0):
-                # exclude index columns
-                replaceColumns = [c for c in df.columns if not 'idx' in c]
-                for inputSentence in df['original_sentence'].unique():
-                    myMask = df['original_sentence'] == inputSentence
-                    currentDf = df.loc[myMask].sort_values(by='paraphrase_perplexity')
-                    bestCandidate = currentDf.head(1)
-                    df.loc[myMask, replaceColumns] = bestCandidate[replaceColumns].values.tolist()
+                paraphraser = loadParaphraser()
+                print('Generating intermediate paraphrases of {} sentences...'.format(len(input_samples)))
+                for i in tqdm(range(0, len(input_samples), batchSize)):
+                    currentBatch = input_samples[i:i + batchSize]
+                    with torch.cuda.device(0):
+                        currentParaphrases, currentScores = paraphraser.generate_batch(currentBatch, top_p=args.top_p_paraphrase, get_scores=True)
+                        currentPerplexities = 2 ** (-np.array(currentScores))
+                    
+                    output_paraphrase += currentParaphrases
+                    paraphrase_perplexities += currentPerplexities.tolist()
+
+                # create df of intermediate / diverse paraphrases and save it
+                df = pd.DataFrame().assign(source_idx=sourceIndices, sentence_idx=sentenceIndices, original_sentence=input_samples, paraphrase=output_paraphrase, paraphrase_perplexity=paraphrase_perplexities)
+                df = df.sort_values(by=['sentence_idx', 'paraphrase_perplexity'])
+                intermediatePath = csvPath.replace('.csv', '_intermediate.csv')
+                if (os.path.isfile(intermediatePath)):
+                    df.to_csv(intermediatePath, mode='a', header=False)
+                else:
+                    df.to_csv(intermediatePath)
+                print('Saved intermediate / diverse paraphrases in', intermediatePath)
+            
+                # only use the most probable intermediate paraphrase
+                if (args.top_p_paraphrase > 0):
+                    # exclude index columns
+                    replaceColumns = [c for c in df.columns if not 'idx' in c]
+                    for inputSentence in df['original_sentence'].unique():
+                        myMask = df['original_sentence'] == inputSentence
+                        currentDf = df.loc[myMask].sort_values(by='paraphrase_perplexity')
+                        bestCandidate = currentDf.head(1)
+                        df.loc[myMask, replaceColumns] = bestCandidate[replaceColumns].values.tolist()
 
             nextColumn = 'paraphrase'
         else:
             # just use the input samples
-            df = pd.DataFrame().assign(source_idx=sourceIndices, sentence_idx=sentenceIndices, original_sentence=input_samples, paraphrase=output_paraphrase, paraphrase_perplexity=paraphrase_perplexities)
+            df = pd.DataFrame().assign(source_idx=sourceIndices, sentence_idx=sentenceIndices, original_sentence=input_samples, paraphrase=np.nan, paraphrase_perplexity=np.nan)
             nextColumn = 'original_sentence'
 
         print('Stylistically paraphrasing {} sentences...'.format(len(df)))
